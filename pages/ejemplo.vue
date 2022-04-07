50dac35c55c3b40ebb5543c95b86b9f968259411
Marco <marco12195@hotmail.com>
2022-04-06 22:55:05 -0500

    crear otra pagina de ejemplo

--- pages/ejemplo.vue
+++ pages/ejemplo.vue
@@ -1,23 +1,59 @@
-<!-- Please remove this file from your project -->
 <template>
-  <div>
-    
-    <div>
-      
-      <div >
-        
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+    <div class="container">
+        <div class="titulo">
+            <h2>Página 2</h2>
+            <br />
+        </div>
+        <div class="h-row">
+            <div class="logo">
+                <a
+                    href="https://www.segundamano.mx"
+                    title="Segundamano México: Anuncios clasificados gratis"
+                    data-src="https://images.segundamano.mx/api/v1/smmx/images/static/logo_med.svg?rule=web_icon_1x"
+                    lazy="loaded"
+                    style="background-image: url('https://images.segundamano.mx/api/v1/smmx/images/static/logo_med.svg?rule=web_icon_1x');"
+                ></a>
+            </div>
+            <br />
+            <div class="header-container_general-info">
+                <button type="button" class="publish-ad" @click="mostrarModal=true">
+                    <a title="Ingresar">Ingresar</a>
+                </button>
+            </div>
+        </div>
 
-      </div>
-     
+        <ModalIngresar v-model="mostrarModal" @getDatos="getDatos" />
+
+        <div>
+            <p>Resultado:</p>
+            <br />
+            <pre>
+              {{ datosForm }}
+            </pre>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  name: 'Ejemplo'
-}
+    export default {
+        data() {
+            return {
+                mostrarModal: false,
+                datosForm: {},
+            };
+        },
+        watch: {
+            mostrarModal: function () {
+                if (this.mostrarModal) {
+                    this.datosForm = {};
+                }
+            },
+        },
+        methods: {
+            getDatos(datosForm) {
+                this.datosForm = datosForm;
+                this.mostrarModal = false;
+            },
+        },
+    };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -28,8 +28,7 @@
             <p>Resultado:</p>
             <br />
             <pre>
-        {{ datosForm }}
-
+                {{ datosForm }}
             </pre>
         </div>
     </div>
@@ -58,46 +57,3 @@
         },
     };
 </script>
-
-<style scoped>
-    .titulo {
-        text-align: center;
-    }
-
-    .h-row {
-        display: flex;
-        margin: 1rem auto;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        align-items: center;
-        position: relative;
-        min-height: 5.5rem;
-        width: 100%;
-        max-width: 1024px;
-    }
-
-    .h-row .publish-ad {
-        cursor: pointer;
-        background: #ff5859;
-        font-size: 1.4rem;
-        border: 0;
-        margin-left: 2.4rem;
-        border-radius: 2px;
-        box-shadow: 0 0.1rem 0.2rem 0 rgb(0 0 0 / 30%);
-        font-weight: 700;
-    }
-    .h-row .publish-ad a {
-        color: #fff;
-        text-decoration: none;
-        padding: 1rem;
-        display: inline-block;
-    }
-
-    .logo a {
-        background-size: cover;
-        background-repeat: no-repeat;
-        float: left;
-        width: 21rem;
-        height: 3rem;
-    }
-</style>
